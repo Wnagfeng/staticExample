31c615e60b311728f61253a49e5b80805dd40291
CoderJoon <14905029+stained-with-dust@user.noreply.gitee.com>
2024-09-25 05:09:03 +0800

    完成

--- README.md
+++ README.md
@@ -1,19 +1,38 @@
-# staticexample
+# 文件上传和展示图片——前端
 
-## Project setup
 ```
-yarn install
-```
-
-### Compiles and hot-reloads for development
-```
-yarn serve
+Write By CoderJoon
+Time: 2024-09-25-05:06
 ```
 
-### Compiles and minifies for production
-```
-yarn build
+
+
+| 环境类型     | 版本号   |
+| ------------ | -------- |
+| Node         | ^14.17.6 |
+| `Elemenu—Ui` | ^2.12.0  |
+| `Vue—Router` | ^3.0.1   |
+| `Vue`        | ^2.5.2`  |
+| `Npm`        | 6.14.15  |
+
+关于图片的展示，很简单，但是zbf不干人事，他返回的图片不是http开头的图片链接，所以你在展示图片的时候，你一定要做一个url的拼接
+
+```vue
+ <div class="imglist">
+        <img v-for="(img, index) in imageList" 
+        :key="index" 
+        :src="`http://localhost:3001/uploads/${img}`"
+        class="image-item" />
+  </div>
 ```
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+这里我就做了链接的拼接 `http://localhost:3001/uploads/xxx.jpg`，zbf返回的很有可能是一个图片名称，所以你需要使用baseurl加文件名称去展示图片，这里可以看看pc端的 shopOrder.vue里面的第八行代码 ，就是一个很好的案例！
+
+如果你略懂Node以及http的知识，这点对于你来说就是小case。
+
+
+
+
+
+
+
--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,125 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div class="Wrapper">
+    <h1 style="text-align: center;">静态资源解决方案</h1>
+    <div class="exampleone" style="text-align: center;">
+      <h3>图片上传实例</h3>
+      <el-upload class="avatar-uploader" action="http://localhost:3001/upload" name="file" :show-file-list="false"
+        :on-success="handleAvatarSuccess" :before-upload="beforeAvatarUpload">
+        <img v-if="imageUrl" :src="imageUrl" class="avatar" />
+        <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+      </el-upload>
     </div>
-    <router-view/>
+
+    <div class="exampleone" style="text-align: center;">
+      <h3>图片的展示示例</h3>
+      <el-button type="success" @click="fetchImageList">请求图片</el-button>
+      <div class="imglist">
+        <img v-for="(img, index) in imageList" :key="index" :src="`http://localhost:3001/uploads/${img}`"
+          class="image-item" />
+      </div>
+    </div>
   </div>
 </template>
 
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      imageUrl: '',
+      imageList: [] // 用于存储图片名称的数组
+    };
+  },
+  methods: {
+    handleAvatarSuccess(res, file) {
+      console.log(res, file);
+      if (res.code === 200) {
+        this.imageUrl = res.url;
+        this.$message.success('上传成功');
+        this.fetchImageList()
+      }
+    },
+    beforeAvatarUpload(file) {
+      const isJPG = file.type === 'image/jpeg';
+      const isLt2M = file.size / 1024 / 1024 < 2;
+
+      if (!isJPG) {
+        this.$message.error('上传头像图片只能是 JPG 格式!');
+      }
+      if (!isLt2M) {
+        this.$message.error('上传头像图片大小不能超过 2MB!');
+      }
+      return isJPG && isLt2M;
+    },
+    fetchImageList() {
+      axios.get('http://localhost:3001/imglist')
+        .then(response => {
+          if (response.data.code === 200) {
+            this.imageList = response.data.images; // 更新图片名称列表
+          } else {
+            this.$message.error('获取图片列表失败');
+          }
+        })
+        .catch(error => {
+          console.error(error);
+          this.$message.error('请求发生错误');
+        });
+    }
+  }
+}
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+.Wrapper {
+  padding: 10px;
+  box-sizing: border-box;
 }
 
-#nav {
-  padding: 30px;
+.exampleone {
+  margin-top: 20px;
+  border: 1px solid #eee;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+  .avatar-uploader .el-upload {
+    border: 1px dashed #d9d9d9;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+  }
 
-    &.router-link-exact-active {
-      color: #42b983;
+  .avatar-uploader .el-upload:hover {
+    border-color: #409EFF;
+  }
+
+  .avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 178px;
+    height: 178px;
+    line-height: 178px;
+    text-align: center;
+  }
+
+  .avatar {
+    width: 178px;
+    height: 178px;
+    display: block;
+  }
+
+  .imglist {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+
+    .image-item {
+      margin: 10px;
+      width: 150px;
+      /* 或者你想要的其他宽度 */
+      height: 150px;
+      /* 或者你想要的其他高度 */
+      object-fit: cover;
+      /* 确保图片不会变形 */
     }
   }
 }
